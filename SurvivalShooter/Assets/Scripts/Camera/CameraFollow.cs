49b262671fab08068f50daffbe89e3272377ea98
Jacob Walker <18JWalker@cpsd.us>
2017-02-03 12:25:07 -0500

    Camera Setup halfway done

---
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	void Start(){
+		offset = transform.position - target.position;
+	}
+
+}
