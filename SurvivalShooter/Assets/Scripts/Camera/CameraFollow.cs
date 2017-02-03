9d2107a5fbaf9b66dda91d716ead2e65d023ab8c
Jacob Walker <18JWalker@cpsd.us>
2017-02-03 12:35:29 -0500

    Camera finished

--- SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -13,4 +13,8 @@ public class CameraFollow : MonoBehaviour {
 		offset = transform.position - target.position;
 	}
 
+	void FixedUpdate(){
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime); 
+	}
 }
